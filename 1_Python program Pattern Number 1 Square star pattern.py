fa3406fb443437e7bf1d30647cb35087ccea4790
Manoj Upadhya <70696140+manojupadhya11@users.noreply.github.com>
2024-04-18 21:35:05 +0530

    Add files via upload

---
+++ 1_Python program Pattern Number 1 Square star pattern.py
@@ -0,0 +1,15 @@
+#Python program to print below pattern
+"""
+   ****
+   ****
+   ****
+   ****
+
+"""
+
+number = 4 #can take user input as well
+
+for i in range(number):
+    for j in range(number):
+        print("*", end = "")
+    print("")
---
+++ 2_Python program Pattern Number 2 Rectangle Square Pattern.py
@@ -0,0 +1,16 @@
+#Python program to print below pattern
+"""
+   ******
+   ******
+   ******
+   ******
+
+"""
+
+number = 4 #can take user input as well
+number2 = 6 #can take user input as well
+
+for i in range(number):
+    for j in range(number2):
+        print("*", end = "")
+    print("")
